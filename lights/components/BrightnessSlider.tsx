4a9541ddc72ddd0186aa0828b62f2bb3821cc7e8
Branimir Kirilov <bkirilov@shutterfly.com>
2023-04-17 19:38:11 +0300

    Extract brightness slider to a standalone component

---
+++ lights/components/BrightnessSlider.tsx
@@ -0,0 +1,70 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { Slider } from '@miblanchard/react-native-slider';
+import { StyleSheet, View, Text } from 'react-native';
+import { LightSourceData } from '../types/types';
+import { Colors } from '../utils/colors';
+
+interface BrightnessSliderProps {
+    onBrightnessChange: (value: number[]) => void
+    item: LightSourceData
+}
+
+export default function BrightnessSlider({ item, onBrightnessChange }: BrightnessSliderProps) {
+    return (
+        <View style={styles.brightnessContainer}>
+            <MaterialIcons
+                name="brightness-4"
+                size={24}
+                color={item.enabled ? item.color : Colors.GRAY}
+            />
+            <Slider
+                containerStyle={styles.sliderStyle}
+                value={item.brightness}
+                onSlidingComplete={onBrightnessChange}
+                step={1}
+                minimumValue={1}
+                maximumValue={100}
+                trackStyle={styles.trackStyle}
+                thumbStyle={styles.thumbStyle}
+                minimumTrackStyle={{
+                    backgroundColor: item.enabled
+                        ? item.color
+                        : Colors.GRAY
+                }}
+            />
+            <Text style={styles.text}>
+                {Math.round(item.brightness || 0)}
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    brightnessContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        bottom: 0
+    },
+    sliderStyle: {
+        width: '80%'
+    },
+    trackStyle: {
+        backgroundColor: Colors.GRAY,
+        height: 25,
+        borderRadius: 10
+    },
+    thumbStyle: {
+        opacity: 0,
+        width: 0
+    },
+    minimumTrackDisabled: {
+        backgroundColor: Colors.GRAY_LIGHT
+    },
+    // TODO: Extract generic styles
+    text: {
+        color: Colors.WHITE,
+        fontFamily: 'sans-serif-thin',
+        fontSize: 18
+    },
+});
--- lights/components/LightSource.tsx
+++ lights/components/LightSource.tsx
@@ -1,12 +1,11 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Slider } from '@miblanchard/react-native-slider';
-import { MaterialIcons } from '@expo/vector-icons';
 import PowerIcon from './PowerIcon';
 import { LightSourceData } from '../types/types';
 import { useAppDispatch } from '../hooks/hooks';
 import { updateLight } from '../store/lights/lightsSlice';
 import { Colors } from '../utils/colors';
 import LoadingOverlay from './LoadingOverlay';
+import BrightnessSlider from './BrightnessSlider';
 
 interface LightSourceProps {
     item: LightSourceData;
@@ -16,6 +15,7 @@ interface LightSourceProps {
 export default function LightSource({ item, onExpand }: LightSourceProps) {
     const dispatch = useAppDispatch();
 
+    // TODO: Better way to do this?
     const toggleEnabled = async () => {
         if (item.enabled) {
             dispatch(
@@ -35,12 +35,11 @@ export default function LightSource({ item, onExpand }: LightSourceProps) {
         );
     };
 
-    const changeBrightness = async (val: number[]) => {
-        console.log('hey', item.id, val[0]);
+    const onBrightnessChange = (value: number[]) => {
         dispatch(
             updateLight({
                 id: item.id,
-                changes: { enabled: true, brightness: val[0] }
+                changes: { enabled: true, brightness: value[0] }
             })
         );
     };
@@ -74,31 +73,7 @@ export default function LightSource({ item, onExpand }: LightSourceProps) {
                         onToggle={toggleEnabled}
                     />
                 </View>
-                <View style={styles.brightnessContainer}>
-                    <MaterialIcons
-                        name="brightness-4"
-                        size={24}
-                        color={item.enabled ? item.color : Colors.GRAY}
-                    />
-                    <Slider
-                        containerStyle={styles.sliderStyle}
-                        value={item.brightness}
-                        onSlidingComplete={changeBrightness}
-                        step={1}
-                        minimumValue={1}
-                        maximumValue={100}
-                        trackStyle={styles.trackStyle}
-                        thumbStyle={styles.thumbStyle}
-                        minimumTrackStyle={{
-                            backgroundColor: item.enabled
-                                ? item.color
-                                : Colors.GRAY
-                        }}
-                    />
-                    <Text style={styles.text}>
-                        {Math.round(item.brightness || 0)}
-                    </Text>
-                </View>
+                <BrightnessSlider item={item} onBrightnessChange={onBrightnessChange} />
                 {item.status === 'loading' && <LoadingOverlay />}
             </TouchableOpacity>
         </View>
@@ -132,27 +107,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center'
     },
-    brightnessContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        bottom: 0
-    },
-    sliderStyle: {
-        width: '80%'
-    },
-    trackStyle: {
-        backgroundColor: Colors.GRAY,
-        height: 25,
-        borderRadius: 10
-    },
-    thumbStyle: {
-        opacity: 0,
-        width: 0
-    },
-    minimumTrackDisabled: {
-        backgroundColor: Colors.GRAY_LIGHT
-    },
+  
     overlayContainer: {
         position: 'absolute',
         bottom: 0
